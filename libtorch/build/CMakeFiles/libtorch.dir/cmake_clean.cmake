6f56aa93d1490fbd9d6caa9572886a166e16fa41
wlk <15938712580@163.com>
2022-01-09 18:03:48 +0800

    pytorch_model_c++

---
+++ libtorch/build/CMakeFiles/libtorch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libtorch.dir/libtorch.cpp.o"
+  "libtorch"
+  "libtorch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libtorch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
