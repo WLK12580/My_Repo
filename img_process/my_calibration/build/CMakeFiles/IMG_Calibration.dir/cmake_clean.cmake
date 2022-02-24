a4d37b9c9d3a2874b3291de8ba99207586869fcf
wlk <15938712580@163.com>
2022-02-24 15:53:32 +0800

    calibation

---
+++ img_process/my_calibration/build/CMakeFiles/IMG_Calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IMG_Calibration.dir/claibration_img.cpp.o"
+  "IMG_Calibration"
+  "IMG_Calibration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IMG_Calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
